d6b50a93bbe2495ab2902f9bbd90e79fc1de3dcd
syntaxsmurf <osdonline@gmail.com>
2020-11-25 23:45:57 +0100

    test through vs code

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from rich.console import Console
 from rich.table import Table
+from rich.traceback import install
 from tinydb import TinyDB, Query
+install() #better error handling.
 
 #Database
-db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': '))
+db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': ')) #for dev
+#db = TinyDB('db.json') for production 
 
 #Rich table
 #todo_list_table = Table()
 #todo_list_table.add_column("Todo", style="bold")
 #todo_list_table.
-
-db.insert({'type': 'apple', 'count': 7})
-db.insert({'type': 'peach', 'count': 3})
