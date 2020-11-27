c20b6414dec3ce7a455ee189b875c90327168cf2
syntaxsmurf <osdonline@gmail.com>
2020-11-27 22:09:50 +0100

    cleaned up a bit

--- main.py
+++ main.py
@@ -11,14 +11,12 @@ db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': ')) #for de
 
 #function to insert into db
 def db_insert(task, status, completed_by):
-    task_id = 0 #this is where you stopped next up making task IDs
-    db.insert({"id": task_id, "task": task, "status": status, "completed_by": completed_by})
+    db.insert({"task": task, "status": status, "completed_by": completed_by, "created": "creation date here"})
 
 #init logic & prompt for user entry
-
 init_menu = Prompt.ask("Welcome user please select your destination (A)dd task, (R)emove task, (V)iew Tasks", choices=["A", "R", "V"])
 
-
+#adds to db.json
 if (init_menu == "A"):
     task_name = input("Great, please name the task you would like to add!: ")
     task_status = "Pending"
@@ -26,17 +24,18 @@ if (init_menu == "A"):
 
     db_insert(task_name, task_status, task_complete_by)
 
+#removes from db.json
 if (init_menu == "R"):
     print("r")
-    
+#View db.json   
 if (init_menu == "V"):
     table = Table(title="Todo list")
-    table.add_column("Item ID", justify="right", style="green", no_wrap=True)
+    table.add_column("Item ID", justify="right", style="green")
     table.add_column("Task", style="green")
     table.add_column("Do date", style="green")
     table.add_column("Status", style="green", justify="right")
     console = Console()
 
     for item in db:
-        table.add_row(str(item.doc_id), item["task"], item["completed_by"], item["status"]) #need to find the right command for pulling data out of tinyDB into these example strings
+        table.add_row(str(item.doc_id), item["task"], item["completed_by"], item["status"])
     console.print(table)
