2b2d2579cab3fa15ed65e8ae8229c2f55d875490
syntaxsmurf <osdonline@gmail.com>
2020-11-28 18:59:34 +0100

    refactoring for type setting. Pretty cool

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': ')) #for de
 
 #function to insert into db
 def db_insert(task: str, status: str, completed_by: str):
-    created_when = str(date.today())
+    created_when: str = str(date.today()) #Must be stringified or tinyDB throws a fit.
     db.insert({"task": task, "status": status, "completed_by": completed_by, "created": created_when})
 
 #reuseable initmenu the type setting is probably not necessary could be like list_todo()
 def menu(a: str, r: str, v: str) -> str:
-    init_menu = Prompt.ask("Welcome user please select your destination (A)dd task, (R)emove task, (V)iew Tasks", choices=[a, r, v])
-    return init_menu
+    prompt: str = Prompt.ask(f"please select your destination {a} to Add task, {r} to Remove task, {v} to View Tasks", choices=[a, r, v])
+    return prompt
 
 #reuseable listing todo
 def list_todo():
@@ -33,13 +33,14 @@ def list_todo():
         table.add_row(str(item.doc_id), item["task"], item["completed_by"], item["status"])
     console.print(table)
 
+print("Welcome user")
 init_menu = menu("A", "R", "V")
 
 #adds to db.json
 if (init_menu == "A"):
-    task_name = input("Great, please name the task you would like to add!: ")
-    task_status = "Pending"
-    task_complete_by = input("When should this task be done by? (example: 30th of december 2020): ")
+    task_name: str = input("Great, please name the task you would like to add!: ")
+    task_status: str = "Pending"
+    task_complete_by: str = input("When should this task be done by? (example: 30th of december 2020): ")
 
     db_insert(task_name, task_status, task_complete_by)
 
