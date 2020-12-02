f5fb4f026c97512695a968e53cada52b93ba9d2f
syntaxsmurf <osdonline@gmail.com>
2020-12-02 21:34:27 +0100

    Reading PIP 8 refactoring code to Pythonic standards

--- main.py
+++ main.py
@@ -4,41 +4,57 @@ from rich.traceback import install
 from rich.prompt import Prompt
 from tinydb import TinyDB, Query
 from datetime import date
-install() #better error handling. from rich.traceback
+install() # better error handling. from rich.traceback
 
-#Database
+# Database
 DB = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': ')) #for dev
-#db = TinyDB('db.json') for "production"
+#db = TinyDB('db.json') for "production"py
 
 #function to insert into db
 def db_insert(task: str, status: str, completed_by: str):
-    created_when: str = str(date.today()) #Must be stringified or tinyDB throws a fit.
-    DB.insert({"task": task, "status": status, "completed_by": completed_by, "created": created_when})
+    #Must be stringified or tinyDB throws a fit.
+    created_when: str = str(date.today())
+    DB.insert({
+        "task": task,
+        "status": status,
+        "completed_by": completed_by,
+        "created": created_when
+    })
 
-#reuseable initmenu the type setting is probably not necessary could be like list_todo()
+# reuseable initmenu the type setting
+# is probably not necessary could be like list_todo()
 def menu(a: str, r: str, v: str) -> str:
-    prompt: str = Prompt.ask(f"please select your destination {a} to Add task, {r} to Remove task, {v} to View Tasks", choices=[a, r, v])
+    prompt: str = Prompt.ask(
+        f"please select your destination {a} to Add task,{r} to Remove task, \
+        {v} to View Tasks", choices=[a, r, v]
+    )
     return prompt
 
 #reuseable listing todo
 def list_todo():
-    table = Table(title="Todo list")
-    table.add_column("Item ID", justify="right", style="green")
-    table.add_column("Task", style="green")
-    table.add_column("Do date", style="green")
+    table = Table(title="Todo list", style="blue")
+    table.add_column("Item ID", style="red", justify="right")
+    table.add_column("Task", style="blue")
+    table.add_column("Do date", style="blue")
     table.add_column("Status", style="green", justify="right")
     console = Console()
     
     for item in sorted(DB, key = lambda i: int(i.doc_id)):
-    #Lambda is like an inline function see below it's the same putting this here for my own reference and learning.
+    # Lambda is like an inline function see below it's the same putting 
+    # this here for my own reference and learning.
     #def foo(i):
     #   return int(i.doc_id)
     #   for item in sorted(db, key = foo):
-        table.add_row(str(item.doc_id), item["task"], item["completed_by"], item["status"])
+        table.add_row(
+            str(item.doc_id),
+            item["task"],
+            item["completed_by"],
+            item["status"]
+        )
 
     console.print(table)
 
-#removing entry this should probably be sanitized somehow but I am not sure.
+# removing entry this should probably be sanitized somehow but I am not sure.
 def db_remove(prompt: str):
         usr_prompt = input(prompt)
         id = int(usr_prompt)
@@ -47,9 +63,12 @@ def db_remove(prompt: str):
         except Exception as e:
             print(f"Your task was not added to do an unforeseen error:{e}")
 
-#this is the main program
+def db_update():
+    return False
+# this is the main program
 def main(user_message: str):
-    #this is how we exit the loop call exit_program("message", "affirmative", "negative") after task with
+    # this is how we exit the loop call
+    # exit_program("message", "affirmative", "negative") after task with
     def exit_program(m: str, y: str, n: str) -> bool:
         exit_prompt: str = Prompt.ask(f"{m}", choices=[y, n])
         if (exit_prompt == y):
@@ -65,12 +84,18 @@ def main(user_message: str):
 
         #adds to db.json
         if (init_menu == "A"):
-            task_name: str = input("Great, please name the task you would like to add!: ")
+            task_name: str = input(
+                "Great, please name the task you would like to add!: "
+            )
             task_status: str = "Pending"
-            task_complete_by: str = input("When should this task be done by? (example: 30th of december 2020): ")
+            task_complete_by: str = input(
+                "When should this task be done by? (example: 30th of december 2020): "
+            )
             try:
                 db_insert(task_name, task_status, task_complete_by)
-                run_program = exit_program("Success! Task has been added. Exit?", "Yes", "No")
+                run_program = exit_program(
+                    "Success! Task has been added. Exit?", "Yes", "No"
+                )
             except Exception as e:
                 print(f"Your task was not added to do an unforeseen error:{e}")
 
@@ -78,7 +103,9 @@ def main(user_message: str):
         if (init_menu == "R"):
             list_todo()
             db_remove("please type in the ID of the task you want to delete: ")
-            run_program = exit_program("Success! Task have been removed. Exit?", "Yes", "No")
+            run_program = exit_program(
+                "Success! Task have been removed. Exit?","Yes", "No"
+            )
         #View db.json   
         if (init_menu == "V"):
             list_todo()
